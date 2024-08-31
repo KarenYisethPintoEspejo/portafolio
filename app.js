8ce01993c200655070ac9d1f1c2e51747e0a307d
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com.>
2024-08-31 16:45:11 -0500

    feat: :sparkles: Se realizo la conexion a express

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+
+const config = {
+    port: process.env.EXPRESS_PORT,
+    host: process.env.EXPRESS_HOST,
+    static: process.env.EXPRESS_STATIC
+};
+
+app.use(express.static(config.static));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(config.static, 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        status: err.status || 500,
+        message: err.message || 'Error interno del servidor'
+    });
+});
+
+app.listen(config.port, config.host, () => {
+    console.log(`http://${config.host}:${config.port}`);
+});
