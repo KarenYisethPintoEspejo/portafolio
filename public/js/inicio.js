a39e774bb6a06fb904582bb2e564b1e42261e3f9
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com.>
2024-09-04 03:05:52 -0500

    feat: :construction: Se comenzo con la construccion de una pagina de carga

---
+++ public/js/inicio.js
@@ -0,0 +1,4 @@
+        // Simula el tiempo de carga
+        setTimeout(() => {
+            window.location.href = ''; // Redirige a la página de destino
+        }, 10000); // 3 segundos de carga
